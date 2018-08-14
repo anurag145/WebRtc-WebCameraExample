db4e2e5bd085bfaf8e681f407fa3e77b4d4b7834
anurag145@gmail.com <anurag145@gmail.com>
2018-08-14 20:46:57 +0530

    v1.0

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {createStore,applyMiddleware} from 'redux'
+import {createStore} from 'redux'
 import {Provider} from 'react-redux';
 import reducer from './store/reducer';
 import registerServiceWorker from './registerServiceWorker';
